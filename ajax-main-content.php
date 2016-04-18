ec83f32b98d95fb6360e6f31ec86e2a4921966f3
Jeff Jenkins <jeff@quantastical.com>
2016-04-17 22:11:19 -0400

    Fixed AJAX loading excerpts
    Fixed issue where AJAX-loaded content would only load the post excerpt.
    AJAX-loaded content now correctly loads the full page content.

--- ajax-main-content.php
+++ ajax-main-content.php
@@ -1,3 +1,7 @@
+<?php
+global $more;
+$more = 1;
+?>
 <main class="<?php echo $post->post_type; ?> row around-xs" id="<?php echo basename( get_permalink( $post->ID ) ); ?>">
 	<article id="<?php echo $post->post_type; ?>-<?php the_ID(); ?>" <?php post_class('col-xs-11 col-sm-9 col-md-7'); ?>>
 <?php get_template_part( $post->post_type, 'header' ); ?>
