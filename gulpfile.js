8dc05ede75d2cd88ab72b954f294c8dff4e65481
Jeff Jenkins <jeff@quantastical.com>
2016-05-02 19:26:55 -0400

    Added SASS compilation to task runner
    Added a SASS compilation task to the Gulp task runner that generates
    the combined and minified versions of the stylesheet.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,34 @@
 var gulp = require('gulp'),
+	rename = require('gulp-rename'),
 	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
 	uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
 });
 
 gulp.task('sass', function() {
-	return gulp
-		.src('style.scss')
+	/*
+	'style.css': 'style.scss',
+	//'editor-style.css': 'editor-style.scss'
+	'styles/admin/style.css': 'styles/admin/style.scss',
+	'styles/editor/style.css': 'styles/editor/style.scss',
+	'styles/login/style.css': 'styles/login/style.scss',
+	'styles/xsl/xml-sitemap.css': 'styles/xsl/xml-sitemap.scss'
+	*/
+
+	gulp.src('style.scss')
+		.pipe(sourcemaps.init())
 		.pipe(sass())
-		.pipe(gulp.dest('style.css'));
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./'));
+
+	return gulp.src('style.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(rename('style.min.css'))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./'));
 });
 
 gulp.task('uglify', function() {
