5c1ed340884262b2794a65f8fe93152f7bfbd26b
Jeff Jenkins <jeff@quantastical.com>
2016-08-06 21:49:25 -0400

    Improved Share button
    Changed the look of the Share buttons for pages and posts, mimicking
    the Material Design floating action buttons. Also, added automatic
    focus & selection to the share link when opening the share modal.

---
+++ scripts/sharing.js
@@ -0,0 +1,28 @@
+jQuery(function($){
+	Urb.scrollSharing = function() {
+		$('main').each(function() {
+			var $main = $(this);
+			if( Urb.scrollPosition + Urb.$window.height() > $main.offset().top + Urb.$pageNavigation.outerHeight() * 1.5
+			 && Urb.scrollPosition + Urb.$window.height() < $main.offset().top + $main.outerHeight() ) {
+				$main.find('.page-share, .post-share').addClass('visible');
+			} else {
+				$main.find('.page-share, .post-share').removeClass('visible');
+			}
+		});
+	};
+
+	Urb.setupSharing = function() {
+		var $main = $('main');
+
+		if($main.length > 0) {
+			$('.page-share, .post-share', $main)
+				.addClass('visible')
+				.bind('click', function() {
+					$('.modal.share-modal .shortlink', $main).focus().select();
+				});
+		}
+	}
+
+	Urb.$window.on('ajaxload load scroll', Urb.scrollSharing);
+	Urb.$window.on('ajaxload load', function() { setTimeout(Urb.setupSharing, 500); });
+});
