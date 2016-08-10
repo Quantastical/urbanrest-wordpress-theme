8e6c63d700f6099c1877255750dea5e69226265c
Jeff Jenkins <jeff@quantastical.com>
2016-08-09 21:15:16 -0400

    Added Custom 404 Page
    Added a custom 404 Page that displays a list of potentially similar
    pages for when a requested resource is not found or fails to load. Also
    added a search form template for displaying a list of search results.

--- page-search.php
+++ page-search.php
@@ -1,21 +1,32 @@
+<?
+/*
+Template Name: Search
+*/
+?>
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 <?php the_post(); ?>
+<?php $search_terms = trim( $_GET['q'] ); ?>
 	<main class="page row around-xs">
 		<article id="page-<?php the_ID(); ?>" <?php post_class('col-xs-11 col-sm-9 col-md-7'); ?>>
 			<header class="page-header" role="banner">
-				<h2>
-					<form action="/search" autocomplete="off" class="site-search" method="get">
-						<label for="q">Search results for</label>
-						<input class="search-input" id="q" name="q" value="<?php echo (isset($_GET['q'])) ? $_GET['q'] : ''; ?>" />
-						<button class="search-submit" type="submit"><span class="btn-label">Find Results</span></button>
-					</form>
-				</h2>
+				<h2>Search Results</h2>
 			</header>
 			<div class="page-content">
+				<p>
+					Here is a list of search results that may help you find what you are looking for.
+				</p>
+
+				<p>
+					<form action="/search" autocomplete="off" class="site-search" method="get">
+						<label for="q">Search results for</label>
+						<input class="search-input" id="q" name="q" value="<?php echo $search_terms; ?>" />
+						<button class="search-submit" type="submit"><span class="btn-label">Find Results</span></button>
+					</form>
+				</p>
 <?php
 $args = array(
-	's' => (isset($_GET['q'])) ? $_GET['q'] : ''
+	's' => $search_terms
 );
 $the_query = new WP_Query( $args );
 ?>
