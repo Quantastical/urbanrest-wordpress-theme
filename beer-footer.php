67a19f59040bd78534bda8fb26e6cf1b96541255
Jeff Jenkins <jeff@quantastical.com>
2016-08-09 23:06:20 -0400

    Improved mobile experience
    Updated Shortlink rendering to allow line-wrapping via a [zero-width
    space
    character](http://www.fileformat.info/info/unicode/char/200b/index.htm)
    for better legibility on mobile devices.

--- beer-footer.php
+++ beer-footer.php
@@ -49,8 +49,8 @@ endif;
 					</div>
 					<button class="page-share beer-share hyperlink" data-action="modal" data-target=".modal.share-modal" type="button">Share this page</button>
 					<div class="modal share-modal animated fadeIn">
-						<a class="shortlink" href="<?php echo wp_get_shortlink(); ?>"><?php echo str_replace(stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://', '', wp_get_shortlink()); ?></a>
-						<h5 class="menu-title">Share this page on your favorite social network.</h5>
+						<a class="shortlink" href="<?php echo wp_get_shortlink(); ?>"><?php echo str_replace('/', '/&#8203;', str_replace(stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://', '', wp_get_shortlink())); ?></a>
+						<h5 class="menu-title">Share this page on your favorite social&nbsp;network.</h5>
 						<ul class="menu">
 							<li class="menu-item">
 								<a class="icon facebook" href="http://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" onclick="javacript:share(this);" data-event-action="Share" data-event-label="Facebook"><span class="icon-label">Share this on Facebook</span></a>
