c347d12f8be2ccc02620787cdde84768014fb063
Jeff Jenkins <jeff@quantastical.com>
2016-08-13 13:20:09 -0400

    Added font selection to admin editor
    Updated WordPress admin content editor to include font family and font
    size selection and cleaned up unused fonts.

--- includes/content-editor.php
+++ includes/content-editor.php
@@ -1,9 +1,31 @@
 <?php
-if( !function_exists('urbanrest_tiny_mce_before_init') ) :
-	function urbanrest_tiny_mce_before_init( $init ) {
-		$init['block_formats'] = "Paragraph=p; Heading 1=h2; Heading 2=h3; Heading 3=h4; Heading 4=h5; Heading 5=h6";
-		return $init;
-	}
-endif;
-add_filter('tiny_mce_before_init', 'urbanrest_tiny_mce_before_init');
+
+function urb_mce_buttons_3( $buttons ) {
+	array_unshift( $buttons, 'fontselect' );
+	array_unshift( $buttons, 'fontsizeselect' );
+	//array_push( $buttons, 'styleselect' );
+
+	return $buttons;
+}
+
+function urb_tiny_mce_before_init( $init ) {
+	$init['block_formats'] = "Paragraph=p;"
+	                       . "Heading 1=h2;"
+	                       . "Heading 2=h3;"
+	                       . "Heading 3=h4;"
+	                       . "Heading 4=h5;"
+	                       . "Heading 5=h6;";
+	$init['font_formats'] = "Sans-Serif (Default)='Roboto',Arial,'Helvetica Neue',Helvetica,sans-serif;"
+	                      . "Serif='Roboto Slab',TimesNewRoman,'Times New Roman',Times,Baskerville,Georgia,serif;"
+	                      . "Monospace='Roboto Mono','Courier New',Courier,'Lucida Sans Typewriter','Lucida Typewriter',monospace;"
+	                      . "Urbanrest='Legend M54',Impact,Haettenschweiler,'Franklin Gothic Bold',Charcoal,'Helvetica Inserat','Bitstream Vera Sans Bold','Arial Black',sans-serif;"
+	                      . "Chalkboard='Chalk It Up',Papyrus,fantasy;"
+	                      . "Cursive='Satisfy','Brush Script MT',cursive;"
+	                      . "Kickstarter='RNS Baruta','Arial Rounded MT Bold','Helvetica Rounded',Arial,sans-serif;";
+	return $init;
+}
+
+add_filter('mce_buttons_3', 'urb_mce_buttons_3');
+add_filter('tiny_mce_before_init', 'urb_tiny_mce_before_init');
+
 ?>
